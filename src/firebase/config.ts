9a3ec36c87d02fb281474e14cc739b5d6b618614
Vivian Boyce <vboyce69@gmail.com>
2026-04-05 19:49:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3125423928-66163",
+  "appId": "1:865119751321:web:9afa64ec4b08ad0149bc08",
+  "apiKey": "AIzaSyCKl4bRNJ6D2-8WU7BjcgOS7HgvjfFP8iw",
+  "authDomain": "studio-3125423928-66163.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "865119751321"
+};
