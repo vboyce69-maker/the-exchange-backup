d630b6f9e318231867e31a08f334343b619e7550
Vivian Boyce <vboyce69@gmail.com>
2026-06-06 12:26:53 +0200

    fix: remove server block from capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,11 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.theexchange.app',
   appName: 'The Exchange',
-  webDir: 'out'
-  server: {
-      url:'http://studio-3125423928-66163.web.app',
-      cleartext: true
-      }
+  webDir: 'out',
 };
 
 export default config;
