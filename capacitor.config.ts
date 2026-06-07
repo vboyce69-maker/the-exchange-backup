217bb45524a61df439e2eb6c0e1e1bd5092eb366
Vivian Boyce <vboyce69@gmail.com>
2026-06-07 12:43:44 +0200

    fix: capacitor config syntax fix by Gemini

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,10 @@ const config: CapacitorConfig = {
   appId: 'com.theexchange.app',
   appName: 'The Exchange',
   webDir: 'public',
+  server: {
+      url: 'https://the-exchange-backup--studio-3125423928-66163.europe-west4.hosted.app',
+      cleartext: false
+      }
 };
 
 export default config;
